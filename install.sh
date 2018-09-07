eb65ba2b21ee4977641167c5e9b99c350211559d
podguzovvasily <podguzovvasily@gmail.com>
2018-09-07 19:51:10 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,37 +5,7 @@ sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-c
 sudo chmod +x /usr/local/bin/docker-compose
 mkdir LEMP
 cd LEMP
-mkdir www www/hello.dev mysql logs hosts images images/php && echo "<?php phpinfo();" > www/hello.dev/index.php && touch images/php/php.ini && touch docker-compose.yml && echo -e "# Для начала указываем исходный образ, он будет использован как основа 
-FROM php:7.0-fpm
-# RUN выполняет идущую за ней команду в контексте нашего образа.
-# В данном случае мы установим некоторые зависимости и модули PHP.
-# Для установки модулей используем команду docker-php-ext-install.
-# На каждый RUN создается новый слой в образе, поэтому рекомендуется объединять команды.
-RUN apt-get update && apt-get install -y \\
-        curl \\
-        wget \\
-        git \\
-        libfreetype6-dev \\
-        libjpeg62-turbo-dev \\
-        libmcrypt-dev \\
-        libpng-dev \\
-    && docker-php-ext-install -j$""(nproc)"" iconv mcrypt mbstring mysqli pdo_mysql zip \\
-    && docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \\
-    && docker-php-ext-install -j$""(nproc)"" gd
-\n
-# Куда же без composer'а.
-RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-\n
-# Добавим свой php.ini, можем в нем определять свои значения конфига
-ADD php.ini /usr/local/etc/php/conf.d/40-custom.ini
-\n
-# Указываем рабочую директорию для PHP
-WORKDIR /var/www/html
-\n
-# Запускаем контейнер
-# Из документации: The main purpose of a CMD is to provide defaults for an executing container. These defaults can include an executable,
-# or they can omit the executable, in which case you must specify an ENTRYPOINT instruction as well.
-CMD ["'"'"php-fpm"'"'"]" > images/php/Dockerfile
+mkdir www www/hello.dev mysql logs hosts images images/php && echo "<?php phpinfo();" > www/hello.dev/index.php && touch images/php/php.ini && touch docker-compose.yml
 
 echo -e "# Версия docker-compose
 version: '2'
@@ -56,7 +26,7 @@ services:
         links:\n
             - php
     php:
-        # у нас свой образ для PHP, указываем путь к нему и говорим что его надо собрать
+        # у нас свой образ для PHP
         image: php:7.2-fpm
         # этот образ будет общаться с mysql
         links:\n
