e1ffc9b1e0ae21af35d31492950ac71f64575fe0
podguzovvasily <podguzovvasily@gmail.com>
2019-04-08 22:16:49 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,39 +3,11 @@ sudo apt install docker.io -y
 sudo systemctl start docker
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+echo -n "Enter your domain name: "
+read domain
 mkdir LEMP
 cd LEMP
-mkdir www www/hello.dev mysql logs hosts images images/php && echo "<?php phpinfo();" > www/hello.dev/index.php && touch images/php/php.ini && touch docker-compose.yml && echo -e "# Для начала указываем исходный образ, он будет использован как основа 
-FROM php:7.0-fpm
-# RUN выполняет идущую за ней команду в контексте нашего образа.
-# В данном случае мы установим некоторые зависимости и модули PHP.
-# Для установки модулей используем команду docker-php-ext-install.
-# На каждый RUN создается новый слой в образе, поэтому рекомендуется объединять команды.
-RUN apt-get update && apt-get install -y \\
-        curl \\
-        wget \\
-        git \\
-        libfreetype6-dev \\
-        libjpeg62-turbo-dev \\
-        libmcrypt-dev \\
-        libpng-dev \\
-    && docker-php-ext-install -j$""(nproc)"" iconv mcrypt mbstring mysqli pdo_mysql zip \\
-    && docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \\
-    && docker-php-ext-install -j$""(nproc)"" gd
-\n
-# Куда же без composer'а.
-RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-\n
-# Добавим свой php.ini, можем в нем определять свои значения конфига
-ADD php.ini /usr/local/etc/php/conf.d/40-custom.ini
-\n
-# Указываем рабочую директорию для PHP
-WORKDIR /var/www
-\n
-# Запускаем контейнер
-# Из документации: The main purpose of a CMD is to provide defaults for an executing container. These defaults can include an executable,
-# or they can omit the executable, in which case you must specify an ENTRYPOINT instruction as well.
-CMD ["'"'"php-fpm"'"'"]" > images/php/Dockerfile
+mkdir www www/$domain mysql logs hosts images images/php && echo "<?php phpinfo();" > www/$domain/index.php && touch images/php/php.ini && touch docker-compose.yml
 
 echo -e "# Версия docker-compose
 version: '2'
@@ -46,33 +18,45 @@ services:
         image: nginx:latest
         # маршрутизируем порты
         ports:
-            - "'"'"8000:80"'"'"
+            - "'"'"80:80"'"'"
         # монтируем директории, слева директории на основной машине, справа - куда они монтируются в контейнере
         volumes:
             - ./hosts:/etc/nginx/conf.d
-            - ./www:/var/www
+            - ./www:/var/www/html
             - ./logs:/var/log/nginx\n
+            - ./etc/letsencrypt_docker/live/$domain:/etc/ssl
         # nginx должен общаться с php контейнером
         links:\n
             - php
     php:
-        # у нас свой образ для PHP, указываем путь к нему и говорим что его надо собрать
-        build: ./images/php
+        # используем стабильный официальный образ для PHP
+        image: php:7.2-fpm
         # этот образ будет общаться с mysql
         links:\n
             - mysql
         # монтируем директорию с проектами
         volumes:
-            - ./www:/var/www
+            - ./www:/var/www/html
     mysql:
         image: mariadb
         ports:
             - "'"'"3306:3306"'"'"
         volumes:
             - ./mysql:/var/lib/mysql
-        # задаем пароль для root пользователя
+         # задаем пароль secret для root пользователя
         environment:
             MYSQL_ROOT_PASSWORD: secret
+            
+    certbot:
+        image: certbot/certbot
+        links:\n
+            - nginx
+        volumes:
+            - ./etc/letsencrypt_docker:/etc/letsencrypt
+            - ./www:/var/www/html
+        command: ["renew"]
+        # Request to certificate docker-compose run certbot certonly --webroot -w /var/www/html/$domain -d $domain -d www.$domain
+        
     pma:
       # используем последний стабильный образ phpmyadmin
         image: phpmyadmin/phpmyadmin
@@ -89,10 +73,10 @@ services:
 
 echo -e "server {
     index index.php;
-    server_name hello.dev;
+    server_name $domain;
     error_log  /var/log/nginx/error.log;
     access_log /var/log/nginx/access.log;
-    root /var/www/hello.dev;
+    root /var/www/html/$domain;
     location ~ \.php$ {
         try_files "'$uri'" =404;
         fastcgi_split_path_info ^(.+\.php)(/.+)$;
@@ -102,6 +86,68 @@ echo -e "server {
         fastcgi_param SCRIPT_FILENAME "'$document_root$fastcgi_script_name;'"
         fastcgi_param PATH_INFO "'$fastcgi_path_info'";
     "'}'"
-}" >hosts/hello-dev.conf
+}" >hosts/$domain.conf
 
 sudo docker-compose up -d
+
+docker-compose run certbot certonly --webroot -w /var/www/html/$domain -d $domain -d www.$domain
+docker-compose run certbot certonly --webroot -w /var/www/html/$domain -d $domain -d www.$domain
+
+echo -e "server {
+    index index.php;
+    server_name $domain;
+    error_log  /var/log/nginx/error.log;
+    access_log /var/log/nginx/access.log;
+    root /var/www/html/$domain;
+    location ~ \.php$ {
+        try_files "'$uri'" =404;
+        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        fastcgi_pass php:9000;
+        fastcgi_index index.php;
+        include fastcgi_params;
+        fastcgi_param SCRIPT_FILENAME "'$document_root$fastcgi_script_name;'"
+        fastcgi_param PATH_INFO "'$fastcgi_path_info'";
+    "'}'"
+}
+
+server {
+	listen [::]:443 ssl http2 default_server;
+	listen 443 ssl http2 default_server;
+        server_name $domain www.$domain
+	ssl_certificate /etc/ssl/fullchain1.pem;
+    	ssl_certificate_key /etc/ssl/privkey1.pem;
+	ssl_dhparam /etc/ssl/dh4096.pem;
+	ssl_trusted_certificate /etc/ssl/chain1.pem
+	ssl_session_cache shared:SSL:50m;
+	ssl_session_timeout 5m;
+	ssl_stapling on;
+	ssl_stapling_verify on;
+	ssl_protocols TLSv1.2 TLSv1.3;
+  	ssl_ciphers 'EECDH+ECDSA+AESGCM:EECDH+aRSA+AESGCM:EECDH+ECDSA+SHA512:EECDH+ECDSA+SHA256:ECDH+AESGCM:ECDH+AES256:DH+AESGCM:DH+AES256:!aNULL:!eNULL:!LOW:!RC4:!3DES:!MD5:!EXP:!PSK:!SRP:!DSS';
+	ssl_prefer_server_ciphers on;
+    	"'add_header Strict-Transport-Security "max-age=31536000;";'"
+    	"'add_header Content-Security-Policy-Report-Only "default-src https:; script-src https: 'unsafe-eval' 'unsafe-inline'; style-src https: 'unsafe-inline'; img-src https: data:; font-src https: data:; report-uri /csp-report";'"
+	"'add_header X-Content-Type-Options "nosniff";'"
+    	"'add_header X-Frame-Options "DENY";'"
+    	"'add_header X-XSS-Protection "1; mode=block";'"
+    	location /__cspreporting__ {
+    	access_log /var/log/nginx/report-uri-csp.log;
+  	}
+
+	
+index index.php;
+    server_name $domain;
+    error_log  /var/log/nginx/error.log;
+    access_log /var/log/nginx/access.log;
+    root /var/www/html/$domain;
+    location ~ \.php$ {
+        try_files "'$uri'" =404;
+        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        fastcgi_pass php:9000;
+        fastcgi_index index.php;
+        include fastcgi_params;
+        fastcgi_param SCRIPT_FILENAME "'$document_root$fastcgi_script_name;'"
+        fastcgi_param PATH_INFO "'$fastcgi_path_info'";
+    }
+
+}" >hosts/$domain.conf
